ef8d8288c12d365124b2263f11adf6a8e15e3fea
LEON <dlwotjr2@gmail.com>
2022-04-05 14:23:27 +0900

    오타수정

--- graph/dijkstra_shortest_path.py
+++ graph/dijkstra_shortest_path.py
@@ -1,11 +1,11 @@
 import heapq
-INF = int(ie9)
+INF = int(1e9)
 
 n, m = map(int, input().split())
 start = int(input())
 
 graph = [[] for i in range(n + 1)]
-distancs = [INF] * (n + 1)
+distance = [INF] * (n + 1)
 
 for _ in range(m):
     a, b, c = map(int, input().split())
